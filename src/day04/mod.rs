0fed46d4ea470f4b9dfee21aa5050e574198fcf5
Marcus <marcus.lancaster@store2be.com>
2022-12-04 15:57:49 +0100

    Complete day 4

---
+++ src/day04/mod.rs
@@ -0,0 +1,100 @@
+use std::ops::RangeInclusive;
+
+type Ranges = (RangeInclusive<u32>, RangeInclusive<u32>);
+
+fn get_ranges(range_pair: &str) -> Ranges {
+    let mut ranges = range_pair
+        .trim()
+        .split(',')
+        .map(|range| {
+            let mut range_iter = range
+                .split("-")
+                .map(|val| val.parse::<u32>().unwrap())
+                .take(2);
+            RangeInclusive::new(range_iter.next().unwrap(), range_iter.next().unwrap())
+        })
+        .take(2);
+    (ranges.next().unwrap(), ranges.next().unwrap())
+}
+
+fn is_fully_containing_range(input: &Ranges) -> bool {
+    input.0.clone().all(|section| input.1.contains(&section))
+        || input.1.clone().all(|section| input.0.contains(&section))
+}
+
+fn is_overlapping_range(input: &Ranges) -> bool {
+    input.0.clone().any(|section| input.1.contains(&section))
+}
+
+pub fn part_one(input: &str) -> u32 {
+    input
+        .lines()
+        .map(get_ranges)
+        .filter(is_fully_containing_range)
+        .count() as u32
+}
+
+pub fn part_two(input: &str) -> u32 {
+    input
+        .lines()
+        .map(get_ranges)
+        .filter(is_overlapping_range)
+        .count() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8";
+
+    #[test]
+    fn part_one_works() {
+        assert_eq!(part_one(INPUT), 2)
+    }
+
+    #[test]
+    fn part_two_works() {
+        assert_eq!(part_two(INPUT), 4)
+    }
+
+    #[test]
+    fn get_ranges_works() {
+        const TEST_VALS: [(&str, Ranges); 6] = [
+            ("2-4,6-8", (2..=4, 6..=8)),
+            ("2-3,4-5", (2..=3, 4..=5)),
+            ("5-7,7-9", (5..=7, 7..=9)),
+            ("2-8,3-7", (2..=8, 3..=7)),
+            ("6-6,4-6", (6..=6, 4..=6)),
+            ("2-6,4-8", (2..=6, 4..=8)),
+        ];
+        for (input, expected_output) in TEST_VALS {
+            assert_eq!(get_ranges(input), expected_output);
+        }
+    }
+
+    #[test]
+    fn range_contains_other_range() {
+        const ONE_CONTAINS_ALL_TWO_RANGE: Ranges = (1..=9, 4..=8);
+        const TWO_CONTAINS_ALL_ONE_RANGE: Ranges = (4..=8, 1..=9);
+        const NON_OVERLAPPING_RANGE: Ranges = (2..=3, 4..=5);
+
+        assert!(is_fully_containing_range(&ONE_CONTAINS_ALL_TWO_RANGE));
+        assert!(is_fully_containing_range(&TWO_CONTAINS_ALL_ONE_RANGE));
+        assert!(!is_fully_containing_range(&NON_OVERLAPPING_RANGE))
+    }
+
+    #[test]
+    fn range_overlaps() {
+        const OVERLAPPING_RANGE: Ranges = (1..=5, 5..=9);
+        const NON_OVERLAPPING_RANGE: Ranges = (2..=3, 4..=5);
+
+        assert!(is_overlapping_range(&OVERLAPPING_RANGE));
+        assert!(!is_overlapping_range(&NON_OVERLAPPING_RANGE));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::read_to_string;
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 fn main() {
     println!("### Day 01 ###");
@@ -31,5 +32,14 @@ fn main() {
     println!(
         "> Part 2 solution: {}",
         day03::part_two(&read_to_string("src/data/day03.txt").unwrap())
+    );
+    println!("### Day 04 ###");
+    println!(
+        "> Part 1 solution: {}",
+        day04::part_one(&read_to_string("src/data/day04.txt").unwrap())
+    );
+    println!(
+        "> Part 2 solution: {}",
+        day04::part_two(&read_to_string("src/data/day04.txt").unwrap())
     );
 }
