7879a2ed1d6eedc12f52306ad5cd50c7efff9bd9
Marcus Lancaster <marcus.h.lancaster@gmail.com>
2022-12-12 12:05:44 +0100

    Tidy up problem output formatting in main

--- src/main.rs
+++ src/main.rs
@@ -11,85 +11,52 @@ mod day08;
 mod day09;
 
 fn main() {
-    println!("### Day 01 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day01::part_one(&read_to_string("src/data/day01.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day01::part_two(&read_to_string("src/data/day01.txt").unwrap())
-    );
-    println!("### Day 02 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day02::part_one(&read_to_string("src/data/day02.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day02::part_two(&read_to_string("src/data/day02.txt").unwrap())
-    );
-    println!("### Day 03 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day03::part_one(&read_to_string("src/data/day03.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day03::part_two(&read_to_string("src/data/day03.txt").unwrap())
-    );
-    println!("### Day 04 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day04::part_one(&read_to_string("src/data/day04.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day04::part_two(&read_to_string("src/data/day04.txt").unwrap())
-    );
-    println!("### Day 05 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day05::part_one(&read_to_string("src/data/day05.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day05::part_two(&read_to_string("src/data/day05.txt").unwrap())
-    );
-    println!("### Day 06 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day06::part_one(&read_to_string("src/data/day06.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day06::part_two(&read_to_string("src/data/day06.txt").unwrap())
-    );
-    println!("### Day 07 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day07::part_one(&read_to_string("src/data/day07.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day07::part_two(&read_to_string("src/data/day07.txt").unwrap())
-    );
-    println!("### Day 08 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day08::part_one(&read_to_string("src/data/day08.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day08::part_two(&read_to_string("src/data/day08.txt").unwrap())
-    );
-    println!("### Day 09 ###");
-    println!(
-        "> Part 1 solution: {}",
-        day09::part_one(&read_to_string("src/data/day09.txt").unwrap())
-    );
-    println!(
-        "> Part 2 solution: {}",
-        day09::part_two(&read_to_string("src/data/day09.txt").unwrap())
-    );
+    for (day_index, (part_one_output, part_two_output)) in
+        get_problem_output_strings().iter().enumerate()
+    {
+        println!("########## Day {:02} ##########", day_index + 1);
+        println!("> Part 1 solution: {}", part_one_output);
+        println!("> Part 2 solution: {}", part_two_output);
+    }
+}
+
+fn get_problem_output_strings() -> Vec<(String, String)> {
+    vec![
+        (
+            day01::part_one(&read_to_string("src/data/day01.txt").unwrap()).to_string(),
+            day01::part_two(&read_to_string("src/data/day01.txt").unwrap()).to_string(),
+        ),
+        (
+            day02::part_one(&read_to_string("src/data/day02.txt").unwrap()).to_string(),
+            day02::part_two(&read_to_string("src/data/day02.txt").unwrap()).to_string(),
+        ),
+        (
+            day03::part_one(&read_to_string("src/data/day03.txt").unwrap()).to_string(),
+            day03::part_two(&read_to_string("src/data/day03.txt").unwrap()).to_string(),
+        ),
+        (
+            day04::part_one(&read_to_string("src/data/day04.txt").unwrap()).to_string(),
+            day04::part_two(&read_to_string("src/data/day04.txt").unwrap()).to_string(),
+        ),
+        (
+            day05::part_one(&read_to_string("src/data/day05.txt").unwrap()),
+            day05::part_two(&read_to_string("src/data/day05.txt").unwrap()),
+        ),
+        (
+            day06::part_one(&read_to_string("src/data/day06.txt").unwrap()).to_string(),
+            day06::part_two(&read_to_string("src/data/day06.txt").unwrap()).to_string(),
+        ),
+        (
+            day07::part_one(&read_to_string("src/data/day07.txt").unwrap()).to_string(),
+            day07::part_two(&read_to_string("src/data/day07.txt").unwrap()).to_string(),
+        ),
+        (
+            day08::part_one(&read_to_string("src/data/day08.txt").unwrap()).to_string(),
+            day08::part_two(&read_to_string("src/data/day08.txt").unwrap()).to_string(),
+        ),
+        (
+            day09::part_one(&read_to_string("src/data/day09.txt").unwrap()).to_string(),
+            day09::part_two(&read_to_string("src/data/day09.txt").unwrap()).to_string(),
+        ),
+    ]
 }
